197d6bdc74c78ee9f81d20608224281fffdf240f
Josias Neto <josiasnieto@hotmail.com>
2024-03-21 18:33:31 +0100

    New Project

---
+++ Projects/IMC Calculator/assets/js/script.js
@@ -0,0 +1,35 @@
+let form = document.getElementById('form');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    let weight = Number(document.getElementById('weight').value);
+    let height = Number(document.getElementById('height').value);
+
+    let bmi = (weight / (height * height)).toFixed(2);
+    let value = document.getElementById('value');
+    let description = '';
+
+    value.classList.add('attention');
+
+    document.getElementById('infos').classList.remove('hidden');
+
+    if (bmi < 18.5){
+        description = 'Você está abaixo do peso!';
+    } else if (bmi >= 18.5 && bmi <= 25){
+        description = 'Você está no peso ideal!';
+        value.classList.remove('attention');
+        value.classList.add('normal');
+    } else if (bmi > 25 && bmi <= 30){
+        description = 'Você está com sobrepeso!';
+    } else if (bmi > 30 && bmi <= 35){
+        description = 'Você está com obesidade moderada!';
+    } else if (bmi > 35 && bmi <= 40){
+        description = 'Você está com obesidade severa!';
+    } else{
+        description = 'Você está com obesidade morbida!';   
+    }
+
+    value.textContent = bmi.replace('.',',');
+    document.getElementById('description').textContent = description;
+});
